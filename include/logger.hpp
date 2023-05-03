bb20cb97745a8a19abd8cb2e488d6701accc9e06
Egor Fortov <egorfortov@gmail.com>
2023-05-04 02:31:09 +0300

    1st commit

---
+++ include/logger.hpp
@@ -0,0 +1,214 @@
+#pragma once
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <ctime>              // for current PC time/date
+#include "nlohmann/json.hpp"  // lib for handling JSON
+#include <cstdio>             // lib for deleting files
+#include <sstream>            // lib for handling time output
+#include <cmath>
+#include "constants.hpp"
+
+std::string changeColor_Q(std::string& str) {
+    return "\x1B[93m" + str + "\033[0m";  // yellow by default
+}
+
+std::string STR_NO_DUPLICATION_Q;
+
+struct Data {
+    std::string start_time = "start_time";
+    std::string end_time = "end_time";
+    std::string logs = "logs";
+    std::string comments = "comments";
+};
+
+bool is_empty(std::ifstream& pFile) {
+    return pFile.peek() == std::ifstream::traits_type::eof();
+}
+
+std::string getTimeString(time_t timestamp) {
+    struct tm* timeinfo = localtime(&timestamp);
+    char buffer[80];
+    strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", timeinfo);
+    return std::string(buffer);
+}
+
+
+/**************************************************/
+/********************* LOGGER *********************/
+/**************************************************/
+class Logger {
+public:
+    std::vector<int64_t>
+        logs;  // measurement results are stored here  // stored in nanosecs always!
+    std::vector<std::string> comments;
+    int8_t format = 0;  // 0 => nanosecs; 1 => microsecs; 2 => milisecs; 3 => secs
+    std::time_t start_time, end_time;
+
+    Logger() {
+        auto current_time = std::chrono::system_clock::now();
+        start_time = std::chrono::system_clock::to_time_t(current_time);
+    }
+
+    void changeDimension(int8_t format) {
+        this->format = format;
+    }
+
+    void log(const int64_t& nanoseconds) {
+        logs.push_back(nanoseconds);
+        comments.push_back("-");
+    }
+
+    void log(const int64_t& nanoseconds, const std::string& comment) {
+        logs.push_back(nanoseconds);
+        comments.push_back(comment);
+    }
+
+    int64_t getMax() {
+        return *max_element(std::begin(logs), std::end(logs));
+    }
+
+    int64_t getMin() {
+        return *min_element(std::begin(logs), std::end(logs));
+    }
+
+    void clear_logs() {
+        this->logs.clear();
+    }
+
+    void clear_log_files() {
+        std::remove("log.txt");
+        std::remove("log.csv");
+        std::remove("log.json");
+    }
+
+    void show_all_statistics() {
+        STR_NO_DUPLICATION_Q = "ALL LOGS: \n";
+        std::cout << changeColor_Q(STR_NO_DUPLICATION_Q);
+        for (int64_t& elem : logs) {
+            STR_NO_DUPLICATION_Q = elem + '\n';
+            std::cout << changeColor_Q(STR_NO_DUPLICATION_Q) << '\n';
+        }
+        STR_NO_DUPLICATION_Q = "Those are all logs!\n";
+        std::cout << changeColor_Q(STR_NO_DUPLICATION_Q);
+    }
+
+    ~Logger() {
+        if (logs.empty()) {
+            return;
+        }
+        auto current_time = std::chrono::system_clock::now();
+        end_time = std::chrono::system_clock::to_time_t(current_time);
+
+        std::ofstream out_txt, out_json, out_csv;
+        const std::string file_txt = "log.txt";
+        const std::string file_csv = "log.csv";
+        out_txt.open(file_txt, std::ios::app);  // open for additional writing to a file (previous
+                                                // data in file remains)
+        out_csv.open(file_csv, std::ios::app);  // -||-
+
+        switch (format) {
+            case 1:  // microsecs
+                for (int i = 0; i < logs.size(); ++i) {
+                    logs[i] /= NANOSECS_IN_MICROSEC;
+                }
+            case 2:  // milisecs
+                for (int i = 0; i < logs.size(); ++i) {
+                    logs[i] /= NANOSECS_IN_MILISEC;
+                }
+            case 3:  // secs
+                for (int i = 0; i < logs.size(); ++i) {
+                    logs[i] /= NANOSECS_IN_SEC;
+                }
+        }
+
+        // TXT output (supports SEVERAL launches of program)
+        // output WITH comments
+        if (out_txt.is_open()) {
+            out_txt << "--------------------------------------------------" << '\n';
+            out_txt << "Profiler started at: " << std::ctime(&start_time);
+            out_txt << "Profiler ended at: " << std::ctime(&end_time);
+            out_txt << "----- LOGS -----\n";
+            switch (format) {
+                case 0:
+                    out_txt << "DIMENSION: nanosecs\n";
+                    break;
+                case 1:
+                    out_txt << "DIMENSION: microsecs\n";
+                    break;
+                case 2:
+                    out_txt << "DIMENSION: milisecs\n";
+                    break;
+                case 3:
+                    out_txt << "DIMENSION: secs\n";
+                    break;
+                default:
+                    out_txt << "DIMENSION: unknown\n";
+            }
+            for (size_t i = 0; i < logs.size(); ++i) {
+                out_txt << logs[i] << ' ' << comments[i] << '\n';
+            }
+            out_txt << "----- STATS -----\n";
+            out_txt << "Min value: " << getMin() << " ; Max value: " << getMax() << '\n';
+            out_txt << "--------------------------------------------------\n\n";
+            out_txt.close();
+        } else {
+            STR_NO_DUPLICATION_Q = "FAILED to open " + file_txt + " file\n";
+            std::cerr << changeColor_Q(STR_NO_DUPLICATION_Q);
+        }
+
+        // CSV output (supports SEVERAL launches of program)
+        // output WITHOUT comments
+        if (out_csv.is_open()) {
+            for (int64_t& elem : logs) {
+                out_csv << elem << ';';
+            }
+            out_csv << '\n';
+            out_csv.close();
+        } else {
+            STR_NO_DUPLICATION_Q = "FAILED to open " + file_csv + " file\n";
+            std::cerr << changeColor_Q(STR_NO_DUPLICATION_Q);
+        }
+
+        // JSON output (supports SEVERAL launch of program)
+        // output WITH comments
+        const std::string file_json = "log.json";
+        std::string str_logs, str_comments;
+        for (int i = 0; i < logs.size(); ++i) {
+            str_logs += std::to_string(logs[i]) + ", ";
+            str_comments += comments[i] + ", ";
+        }
+
+        Data new_data = {getTimeString(start_time), getTimeString(end_time), str_logs,
+                         str_comments};
+        std::ifstream file_in(file_json);
+        if (is_empty(file_in)) {
+            std::ofstream out;
+            out.open(file_json);
+            if (out.is_open()) {
+                out << "[]" << std::endl;
+            }
+            out.close();
+        }
+        file_in.close();
+        file_in.open(file_json);
+        nlohmann::json j_in;
+        file_in >> j_in;
+        for (size_t i = 0; i < logs.size(); ++i) {
+            j_in.push_back({// Add new object to JSON array
+                            {"start_time", new_data.start_time},
+                            {"end_time", new_data.end_time},
+                            {"logs", new_data.logs},
+                            {"comments", new_data.comments}});
+        }
+
+        file_in.close();
+
+        // New json is writed to file
+        std::ofstream file_out2(file_json);
+        file_out2 << j_in.dump(4);
+        file_out2.close();
+    }
+};
+// LOGGER ended
---
+++ profiler.hpp
@@ -0,0 +1,207 @@
+#pragma once
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <ctime>              // for current PC time/date
+#include "include/nlohmann/json.hpp"  // lib for handling JSON
+#include <cstdio>             // lib for deleting files
+#include <sstream>            // lib for handling time output
+#include <cmath>
+#include "include/logger.hpp"
+#include "include/constants.hpp"
+
+typedef long long unsigned int ll;
+
+// Определение структуры
+
+
+std::string changeColor(std::string& str) {
+    return "\x1B[93m" + str + "\033[0m";  // yellow by default
+}
+
+std::string getGracefulDoubleString(double str) {
+    int precisionVal = 3;
+    std::string trimmedString =
+        std::to_string(str).substr(0, std::to_string(str).find(".") + precisionVal + 1);
+    return trimmedString;
+}
+
+std::string STR_NO_DUPLICATION;
+
+
+/**************************************************/
+/******************** PROFILER ********************/
+/**************************************************/
+class Profiler {
+public:
+    Logger logger;
+    std::chrono::time_point<std::chrono::high_resolution_clock> startTime, stopTime;
+    std::chrono::duration<ll, std::nano> duration_time;
+};
+
+Profiler profiler;
+double RES_NO_DUPLICATION;
+
+/*************** Beginning of time measurement ***************/
+#define START profiler.startTime = std::chrono::high_resolution_clock::now();
+#define STARTP                                                      \
+    profiler.startTime = std::chrono::high_resolution_clock::now(); \
+    STR_NO_DUPLICATION = "Started...\n";                            \
+    std::cout << changeColor(STR_NO_DUPLICATION);
+#define START_PRINT_LINE STARTP
+#define START_PRINT STARTP
+
+/*************** End of time measurement ***************/
+// Standart:
+#define END                                                                        \
+    profiler.stopTime = std::chrono::high_resolution_clock::now();                 \
+    profiler.duration_time = std::chrono::duration_cast<std::chrono::nanoseconds>( \
+        profiler.stopTime - profiler.startTime);                                   \
+    RES_NO_DUPLICATION = (double)profiler.duration_time.count();                   \
+    profiler.logger.log(RES_NO_DUPLICATION);
+#define ENDP                                                                               \
+    profiler.stopTime = std::chrono::high_resolution_clock::now();                         \
+    profiler.duration_time = std::chrono::duration_cast<std::chrono::nanoseconds>(         \
+        profiler.stopTime - profiler.startTime);                                           \
+    RES_NO_DUPLICATION = (double)profiler.duration_time.count();                           \
+    STR_NO_DUPLICATION =                                                                   \
+        getGracefulDoubleString(RES_NO_DUPLICATION) / NANOSECS_IN_MILISEC + " milisecs\n"; \
+    std::cout << changeColor(STR_NO_DUPLICATION);                                          \
+    profiler.logger.log(RES_NO_DUPLICATION);                                               \
+    // std::cout << "Testing result has been written to a logger file" << '\n';
+#define END_PRINT ENDP
+
+// With comments:
+#define END_WITH_COMMENT(str)                                                          \
+    profiler.stopTime = std::chrono::high_resolution_clock::now();                     \
+    profiler.duration_time = std::chrono::duration_cast<std::chrono::nanoseconds>(     \
+        profiler.stopTime - profiler.startTime);                                       \
+    RES_NO_DUPLICATION = (double)profiler.duration_time.count() / NANOSECS_IN_MILISEC; \
+    \ profiler.logger.log(RES_NO_DUPLICATION, str);                                    \
+    // std::cout << "Testing result has been written to a logger file" << '\n';
+#define END_COMMENT(str) END_WITH_COMMENT(str)
+
+#define ENDP_WITH_COMMENT(str)                                                         \
+    profiler.stopTime = std::chrono::high_resolution_clock::now();                     \
+    profiler.duration_time = std::chrono::duration_cast<std::chrono::nanoseconds>(     \
+        profiler.stopTime - profiler.startTime);                                       \
+    RES_NO_DUPLICATION = (double)profiler.duration_time.count() / NANOSECS_IN_MILISEC; \
+    STR_NO_DUPLICATION = getGracefulDoubleString(RES_NO_DUPLICATION) + " milisecs\n";  \
+    std::cout << changeColor(STR_NO_DUPLICATION);                                      \
+    profiler.logger.log(RES_NO_DUPLICATION, str);                                      \
+    // std::cout << "Testing result has been written to a logger file" << '\n';
+#define END_PRINT_WITH_COMMENT(str) ENDP_WITH_COMMENT(str)
+#define ENDP_COMMENT(str) ENDP_WITH_COMMENT(str)
+#define END_PRINT_COMMENT(str) ENDP_WITH_COMMENT(str)
+
+/*************** Printing result of time measurement ***************/
+// Standart:
+#define PRINT_TIME                                                                              \
+    std::string ANS_NO_DUPLICATION_1 =                                                          \
+        getGracefulDoubleString((double)profiler.duration_time.count() / NANOSECS_IN_MILISEC) + \
+        " milisecs" + " | " + profiler.logger.comments.back() + '\n';                           \
+    std::cout << changeColor(ANS_NO_DUPLICATION_1);
+#define PRINT_TIME_IN_NANOSECS                                                                  \
+    std::string ANS_NO_DUPLICATION_2 =                                                          \
+        getGracefulDoubleString((double)profiler.duration_time.count()) + " nanosecs" + " | " + \
+        profiler.logger.comments.back() + '\n';                                                 \
+    std::cout << changeColor(ANS_NO_DUPLICATION_2);
+#define PRINT_TIME_IN_MICROSECS                                                                  \
+    STR_NO_DUPLICATION =                                                                         \
+        getGracefulDoubleString((double)profiler.duration_time.count() / NANOSECS_IN_MICROSEC) + \
+        " microsecs" + " | " + profiler.logger.comments.back() + '\n';                           \
+    std::cout << changeColor(STR_NO_DUPLICATION);
+#define PRINT_TIME_IN_SECS                                                                  \
+    STR_NO_DUPLICATION =                                                                    \
+        getGracefulDoubleString((double)profiler.duration_time.count() / NANOSECS_IN_SEC) + \
+        " secs" + " | " + profiler.logger.comments.back() + '\n';                           \
+    std::cout << changeColor(STR_NO_DUPLICATION);
+#define PRINT_TIME_IN_MILISECS PRINT_TIME
+
+// With comments (these are temporary comments):
+#define PRINT_TIME_WITH_COMMENT(str)                                                            \
+    STR_NO_DUPLICATION = "Comment: " + str + ":\n";                                             \
+    std::cout << changeColor(STR_NO_DUPLICATION);                                               \
+    STR_NO_DUPLICATION =                                                                        \
+        getGracefulDoubleString((double)profiler.duration_time.count() / NANOSECS_IN_MILISEC) + \
+        " milisecs\n";                                                                          \
+    std::cout << changeColor(STR_NO_DUPLICATION);
+#define PRINT_TIME_IN_NANOSECS_WITH_COMMENT(str)                                         \
+    STR_NO_DUPLICATION = "Comment: " + str + ":\n";                                      \
+    std::cout << changeColor(STR_NO_DUPLICATION);                                        \
+    STR_NO_DUPLICATION =                                                                 \
+        getGracefulDoubleString((double)profiler.duration_time.count()) + " nanosecs\n"; \
+    std::cout << changeColor(STR_NO_DUPLICATION);
+#define PRINT_TIME_IN_MICROSECS_WITH_COMMENT(str)                                                \
+    STR_NO_DUPLICATION = "Comment: " + str + ":\n";                                              \
+    std::cout << changeColor(STR_NO_DUPLICATION);                                                \
+    STR_NO_DUPLICATION =                                                                         \
+        getGracefulDoubleString((double)profiler.duration_time.count() / NANOSECS_IN_MICROSEC) + \
+        " microsecs\n";                                                                          \
+    std::cout << changeColor(STR_NO_DUPLICATION);
+#define PRINT_TIME_IN_SECS_WITH_COMMENT(str)                                              \
+    STR_NO_DUPLICATION = "Comment: " + str + ":\n";                                       \
+    std::cout << changeColor(STR_NO_DUPLICATION);                                         \
+    STR_NO_DUPLICATION =                                                                  \
+        getGracefulDoubleString((double)profiler.duration_time.count() / NANOSECS_IN_SEC) \
+        << " secs\n";                                                                     \
+    std::cout << changeColor(STR_NO_DUPLICATION);
+#define COMMENT(str) PRINT_TIME_WITH_COMMENT(str)
+
+/*************** Profiler info ***************/
+#define INFO                                                                          \
+    STR_NO_DUPLICATION = "------- Simple fortov profiler -------\n";                  \
+    std::cout << changeColor(STR_NO_DUPLICATION);                                     \
+    STR_NO_DUPLICATION =                                                              \
+        "Simple profiler for measuring execution time in .cpp programms;\nCommands: " \
+        "START, END, ENDP(=ENDR=ENDF=ENDf), PRINT_TIME, PRINT_TIME_IN_MICROSECS, "    \
+        "PRINT_TIME_IN_SECS\n";                                                       \
+    std::cout << changeColor(STR_NO_DUPLICATION);                                     \
+    STR_NO_DUPLICATION = "dev's email: ekfortov@edu.hse.ru";                          \
+    std::cout << changeColor(STR_NO_DUPLICATION);
+
+// STOPPED HERE
+/*************** Profiler help ********* ******/
+#define HELP                                                      \
+    STR_NO_DUPLICATION = "Short overview of all commands:\n";     \
+    std::cout << changeColor(STR_NO_DUPLICATION);                 \
+    std::string LINE_NO_DUPLICATION;                              \
+    std::ifstream in_help_NO_DUPLICATION;                         \
+    in_help_NO_DUPLICATION.open("help.txt");                      \
+    if (in_help_NO_DUPLICATION.is_open()) {                       \
+        while (getline(in, LINE_NO_DUPLICATION)) {                \
+            std::cout << LINE_NO_DUPLICATION << '\n';             \
+        }                                                         \
+        in_help_NO_DUPLICATION.close();                           \
+    } else {                                                      \
+        STR_NO_DUPLICATION = "Failed to open help file" std::cerr \
+                             << changeColor(STR_NO_DUPLICATION);  \
+    }
+
+/*************** Profiler history ***************/
+#define SHOW_ALL_LOGS profiler.logger.show_all_statistics();
+
+/************ Profiler clear history ************/
+#define CLEAR_LOGS profiler.logger.clear_logs();
+#define CLEAR_ALL_FILE_LOGS profiler.logger.clear_log_files();
+#define CLEAR_ALL_LOGS            \
+    profiler.logger.clear_logs(); \
+    profiler.logger.clear_log_files();
+
+/* ANALYZER */
+#define ANALYZE                                                       \
+    STR_NO_DUPLICATION = "Use log files to analyze your code speed!"; \
+    std::cout << changeColor(STR_NO_DUPLICATION);
+
+#define CHANGE_DIMENSION(str)   \
+    switch (str) {              \
+        case "NANOSECS":        \
+            CHANGE_DIMENSION(0) \
+        case "MICROSECS":       \
+            CHANGE_DIMENSION(1) \
+        case "MILISECS":        \
+            CHANGE_DIMENSION(2) \
+        case "SECS":            \
+            CHANGE_DIMENSION(3) \
+    }
